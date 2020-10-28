964d2368d3ec4afe2a2991a8595db7c8b4a61d67
Joshua Pianto <p21jpianto@fairfieldprep.org>
2020-10-27 22:23:06 -0400

    hw 6-2 in progress

---
+++ hw6-2.py
@@ -0,0 +1,34 @@
+# Author: JAP (amdg) 10/27/20
+
+import random
+
+random.seed(16)
+
+#question  1
+random.randrange
+
+#question 2
+
+
+#question 3
+
+
+#question 4
+
+
+#question 5
+
+
+#question 6
+
+
+#question 7
+
+
+#question 8
+
+
+#question 9
+
+
+#question 10
